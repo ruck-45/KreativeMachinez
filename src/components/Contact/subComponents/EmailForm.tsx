f64ff85412baf0ba75e8b5449c6d9250cee16a01
AnishMishra1 <124522187+AnishMishra1@users.noreply.github.com>
2024-02-08 17:28:18 +0530

    making form  integrate with backend (#58)
    * making form  integrate with backend
    
    * updating request
    
    * changes done in fnc
    
    * loader set & validation update
    
    * small correction in loader
    
    * update validation
    
    * resolving issue
    
    * write else condition
    
    * add validation
    
    ---------
    
    Co-authored-by: AnishMishra1 <Anishmishra@gmail.com>

--- src/components/Contact/subComponents/EmailForm.tsx
+++ src/components/Contact/subComponents/EmailForm.tsx
@@ -1,52 +1,47 @@
 // Dependencies
 import { Input, Textarea, Button } from "@nextui-org/react";
 import { IoSend } from "react-icons/io5";
-import { useState, useRef, FormEvent } from "react";
-import emailjs from "@emailjs/browser";
+import { FormEvent, useRef, useState } from "react";
 import toast, { Toaster, ToastPosition } from "react-hot-toast";
+import axios from "axios";
+import { send } from "process";
 
 const emailRe: RegExp = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_-]+)(\.[a-zA-Z]{2,5}){1,2}$/;
 const toastSetting: {
   position: ToastPosition;
 } = { position: "top-center" };
 
-const formNotFill = (): string => toast.error("Please Fill The Form Correctly", toastSetting);
-const emailSent = (): string => toast.success("Email Sent", toastSetting);
-const emailNotSent = (): string => toast.error("Email Not Sent", toastSetting);
+let apiUrl = process.env.REACT_APP_API_URL;
+if (process.env.NODE_ENV === "development") {
+  apiUrl = process.env.REACT_APP_DEV_API_URL;
+}
 
 const EmailForm = () => {
   const form = useRef<HTMLFormElement>(null);
-  const email = useRef<string>("");
 
   const [emailValidity, setEmailValidity] = useState<boolean>(false);
+  const [nameValidity, setNameValidity] = useState<boolean>(false);
+  const [subjectValidity, setSubjectValidity] = useState<boolean>(false);
+  const [messageValidity, setMessageValidity] = useState<boolean>(false);
 
   const [emailState, setEmailState] = useState<number>(-1);
   const [userNameState, setUserNameState] = useState<number>(-1);
+  const [subjectState, setSubjectState] = useState<number>(-1);
+  const [messageState, setMessageState] = useState<number>(-1);
+  const [state, setState] = useState(false);
 
- const sendEmail = async () => {
-   try {
-     if (!emailValidity && userNameState > 0 && emailState > 0) {
-       await emailjs.sendForm(
-         `${process.env.REACT_APP_SERVICE_ID}`,
-         `${process.env.REACT_APP_TEMPLATE_ID}`,
-         form.current!,
-         `${process.env.REACT_APP_PUBLIC_KEY}`
-       );
-       emailSent();
-     } else {
-       formNotFill();
-     }
-   } catch (error) {
-     emailNotSent();
-   }
- };
-
+  const [input, setInput] = useState({
+    name: "",
+    message: "",
+    subject: "",
+    email: "",
+  });
 
   const checkEmail = (event: FormEvent<HTMLInputElement>) => {
-    email.current = event.currentTarget.value;
+    input.email = event.currentTarget.value;
     setEmailState(event.currentTarget.value.length);
 
-    const validity = email.current.match(emailRe);
+    const validity = input.email.match(emailRe);
     if (validity) {
       setEmailValidity(false);
     } else {
@@ -54,6 +49,77 @@ const EmailForm = () => {
     }
   };
 
+  const checkUserName = (event: FormEvent<HTMLInputElement>) => {
+    input.name = event.currentTarget.value;
+    setUserNameState(event.currentTarget.value.length);
+
+    const validity = input.name.length > 2;
+    if (validity) {
+      setNameValidity(false);
+    } else {
+      setNameValidity(true);
+    }
+  };
+
+  const checkSubject = (event: FormEvent<HTMLInputElement>) => {
+    input.subject = event.currentTarget.value;
+    setSubjectState(event.currentTarget.value.length);
+
+    const validity = input.subject.length > 2;
+    if (validity) {
+      setSubjectValidity(false);
+    } else {
+      setSubjectValidity(true);
+    }
+  };
+
+  const checkMessage = (event: FormEvent<HTMLInputElement>) => {
+    input.message = event.currentTarget.value;
+    setMessageState(event.currentTarget.value.length);
+
+    const validity = input.message.length > 2;
+    if (validity) {
+      setMessageValidity(false);
+    } else {
+      setMessageValidity(true);
+    }
+  };
+
+  const sendEmail = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if(!input.name || !input.email || !input.message || !input.subject){
+      toast.error("please fill all details")
+      setState(false);
+      return
+    }
+    
+
+    try {
+      setState(true);
+      const response = await axios.post(`${apiUrl}/contact/form`, input);
+
+      if (response?.data?.success) {
+        toast.success("Email sent Successfully");
+        setState(false);
+      }
+      else{
+        toast.error("Email not sent Succesfully");
+        setState(false)
+        return
+      }
+    } catch (error) {
+      toast.error("failed to send Email 404");
+      setState(false);
+    }
+
+    setInput({
+      name: "",
+      message: "",
+      subject: "",
+      email: "",
+    });
+  };
   return (
     <div className="bg-black px-[3rem] md:px-[5rem] py-[5rem] dark flex flex-col lg:flex-row gap-[2rem] lg:gap-[5rem]">
       <div className="text-white lg:w-[40%] flex flex-col gap-[1rem] lg:order-last">
@@ -75,9 +141,10 @@ const EmailForm = () => {
             type="text"
             label="Name"
             name="name"
-            onChange={(event) => setUserNameState(event.currentTarget.value.length)}
-            errorMessage={userNameState === 0 ? "Please enter a valid Name" : ""}
-            isInvalid={userNameState === 0}
+            id="name"
+            onChange={checkUserName}
+            errorMessage={nameValidity ? "Please enter a valid Name" : ""}
+            isInvalid={nameValidity}
           />
           <Input
             type="email"
@@ -88,14 +155,28 @@ const EmailForm = () => {
             errorMessage={emailValidity ? "Please enter a valid Email" : ""}
           />
         </div>
-        <Input type="text" label="Subject" name="subject" />
-        <Textarea label="Message" name="message"/>
+        <Input
+          type="text"
+          label="Subject"
+          name="subject"
+          onChange={checkSubject}
+          isInvalid={subjectValidity}
+          errorMessage={subjectValidity ? "Please enter a valid Subject" : ""}
+        />
+        <Textarea
+          label="Message"
+          name="message"
+          onChange={checkMessage}
+          isInvalid={messageValidity}
+          errorMessage={messageValidity ? "Please enter a minimum 10 charater" : ""}
+        />
         <Button
           color="warning"
           variant="shadow"
           className="w-[10rem]"
           endContent={<IoSend className="mt-[0.2rem]" />}
-          onClick={sendEmail}
+          type="submit"
+          isLoading={state}
         >
           Send Message
         </Button>
